6cfa4a71a963c2b82e8662a56989fc4c0ac3f1ea
ablb12 <ablb12@aim.omaha.lib.ne.us>
2019-08-17 11:53:08 -0500

    woop wop got it working boop bop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
-DankMeme.where(quote: "they call it gorilla glue because harambe was metaphorically the glue that held this nation together.").first_or_create
+DankMeme.where(quote: "They call it gorilla glue because harambe was metaphorically the glue that held this nation together.").first_or_create
 DankMeme.where(quote: "Do you ever just flex your foot wrong and you're like, this is it. This is how it ends.").first_or_create
 DankMeme.where(quote: "Just because you're trash doesn't mean you can't do great things. It's called a garbage can. Not a garbage cannot.").first_or_create
+DankMeme.where(quote: "Looks like you missed your spanish lesson! You know what happens now!").first_or_create
+DankMeme.where(quote: "Beg for your life in spanish.").first_or_create
+DankMeme.where(quote: "If aliens ever raid earth and ask what we do with our technology, they're gonna blow up our planet.").first_or_create
+DankMeme.where(quote: "I was born too late to explore the world. I was born too early to explore the galaxy. I was born in just enought time to explore dANk MEmeS.").first_or_create
+DankMeme.where(quote: "I'd bake you a cake today if I knew how to use an oven.").first_or_create
+DankMeme.where(quote: "Friendship must have a solid base of fun, sarcasm, inappropriateness, and shenanigans.").first_or_create
+DankMeme.where(quote: "You're ridiculous, insane, and inappropriate. In other words. my new best friend.").first_or_create
+DankMeme.where(quote: "Bestie is coming to town and the week will be filled with shenanigans, debauchery and very bad decisions.").first_or_create
+DankMeme.where(quote: "We have to be friends. You know too much about me to live otherwise.").first_or_create
+DankMeme.where(quote: "A friend will calm you down when you're angry, but a BEST friend will skip beside you with a baseball bat singing 'someone's gonna get it'.").first_or_create
+DankMeme.where(quote: "I don't think of you as a coworker due to fact that you never work.").first_or_create
+DankMeme.where(quote: "Hi, I don't care. Thanks.").first_or_create
--- routes.rb
+++ routes.rb
@@ -1,13 +1,5 @@
+
 get '/' do
-  if no_authentication?
-    erb :"home"
-  else
-    "Hi, #{session[:name]}.  <img style='width:60px; border-radius:50%;' src='#{session[:picture]}' >"
-
-  end
-end
-
-get '/quote' do
 	@dankmemes = DankMeme.all
 	erb :quote
 end
